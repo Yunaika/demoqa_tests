ca5a4989eec897b1b7a8907d5c05ec28af93d85c
Julia Ivanova <murova47@gmail.com>
2022-06-29 00:59:17 +0800

    Update test_webtables.py

--- tests/test_webtables.py
+++ tests/test_webtables.py
@@ -1,5 +1,5 @@
+from selene import have, be
 from selene.support.shared import browser
-from selene import be, have
 
 #открываем страницу и скрываем футер
 def given_opened_page_webtables():
@@ -7,36 +7,68 @@ def given_opened_page_webtables():
 
 #добавляем новую строку в таблицу
 def test_table_add_new_row():
-    #открываем страницу
+    # PRECONDITION
     given_opened_page_webtables()
 
-    #добавляем новую строку
+    # WHEN
     browser.element('#addNewRecordButton').click()
-    browser.element('#firstName').should(be.blank).type('Olga')
-    browser.element('#lastName').should(be.blank).type('Semenova')
-    browser.element('#userEmail').should(be.blank).type('123@123.ru')
-    browser.element('#age').should(be.blank).type('30')
-    browser.element('#salary').should(be.blank).type('5000')
-    browser.element('#department').should(be.blank).type('Legal')
+    browser.element('#firstName').type('Olga')
+    browser.element('#lastName').type('Semenova')
+    browser.element('#userEmail').type('123@123.ru')
+    browser.element('#age').type('30')
+    browser.element('#salary').type('5000')
+    browser.element('#department').type('Legal')
     browser.element('#submit').click()
+
+    # THEN
+    path_to_new_row = '//*[@class="rt-table"]//*[@class="rt-tbody"]//*[@role="rowgroup"][4]//*[@class="rt-td"]'
+
+    browser.element(f'{path_to_new_row}[1]').should(have.exact_text('Olga'))
+    browser.element(f'{path_to_new_row}[2]').should(have.exact_text('Semenova'))
+    browser.element(f'{path_to_new_row}[3]').should(have.exact_text('30'))
+    browser.element(f'{path_to_new_row}[4]').should(have.exact_text('123@123.ru'))
+    browser.element(f'{path_to_new_row}[5]').should(have.exact_text('5000'))
+    browser.element(f'{path_to_new_row}[6]').should(have.exact_text('Legal'))
 
 #редактируем все поля во второй строке
 def test_table_edit_row_two():
-    #открываем страницу
+    # RECONDITION
     given_opened_page_webtables()
+    path_to_row = '//*[@class="rt-table"]//*[@class="rt-tbody"]//*[@role="rowgroup"][2]'
+    path_to_column = '//*[@class="rt-td"]'
+    path_to_edit_button = '//*[@title="Edit"]'
 
-    browser.element('//*[@class="rt-table"]//*[@class="rt-tbody"]//*[@role="rowgroup"][2]//*[@title="Edit"]').click()
-    browser.element('#firstName').clear().type('Ivan')
-    browser.element('#lastName').clear().type('Petrov')
-    browser.element('#userEmail').clear().type('321@123.ru')
-    browser.element('#age').clear().type('37')
-    browser.element('#salary').clear().type('2000')
-    browser.element('#department').clear().type('Legal')
+    # WHEN
+    browser.element(path_to_row + path_to_edit_button).click()
+    browser.element('#firstName').set_value('Ivan')
+    browser.element('#lastName').set_value('Petrov')
+    browser.element('#userEmail').set_value('321@123.ru')
+    browser.element('#age').set_value('37')
+    browser.element('#salary').set_value('2000')
+    browser.element('#department').set_value('Legal')
     browser.element('#submit').click()
 
+    # THEN
+    browser.element(f'{path_to_row + path_to_column}[1]').should(have.exact_text('Ivan'))
+    browser.element(f'{path_to_row + path_to_column}[2]').should(have.exact_text('Petrov'))
+    browser.element(f'{path_to_row + path_to_column}[3]').should(have.exact_text('37'))
+    browser.element(f'{path_to_row + path_to_column}[4]').should(have.exact_text('321@123.ru'))
+    browser.element(f'{path_to_row + path_to_column}[5]').should(have.exact_text('2000'))
+    browser.element(f'{path_to_row + path_to_column}[6]').should(have.exact_text('Legal'))
+
 def test_tables_remove_row_three():
-    #открываем страницу
+    # RECONDITION
     given_opened_page_webtables()
 
-    #удаляем третью строку
+    # WHEN
     browser.element('//*[@class="rt-table"]//*[@class="rt-tbody"]//*[@role="rowgroup"][3]//*[@title="Delete"]').click()
+
+    # THEN
+    path_to_row = '//*[@class="rt-table"]//*[@class="rt-tbody"]//*[@role="rowgroup"][3]//*[@class="rt-td"]'
+
+    # browser.element(f'{path_to_row}[1]').should(be.empty)
+    # browser.element(f'{path_to_row}[2]').should(be.empty)
+    # browser.element(f'{path_to_row}[3]').should(be.empty)
+    # browser.element(f'{path_to_row}[4]').should(be.empty)
+    # browser.element(f'{path_to_row}[5]').should(be.empty)
+    # browser.element(f'{path_to_row}[6]').should(be.empty)
