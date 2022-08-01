122d4aecce35e88acd6e94bcc223a67578c3fb12
Julia Ivanova <murova47@gmail.com>
2022-08-01 23:55:09 +0800

    Add Allure Reports + Selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,61 @@
+import os
+import shutil
 import pytest
-from selene.support.shared import browser
+from dotenv import load_dotenv
+from selene import Browser, Config
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from demoqa_tests.utils.attach import add_html, add_screenshot, add_logs#, add_video
 
-@pytest.fixture(scope='function', autouse=True)
-def browser_managment():
-     browser.config.base_url = 'https://demoqa.com'
-     # browser.config.hold_browser_open = 'True'
-     browser.config.window_width = 1600
-     browser.config.window_height = 1000
+DEFAULT_BROWSER_VERSION = '100.0'
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version'
+    )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+    options = Options()
+    selenoid_capabilities = {
+         "browserName": "chrome",
+         "browserVersion": browser_version,
+         "selenoid:options": {
+              "enableVNC": True,
+              "enableVideo": True
+         }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
+    driver = webdriver.Remote(
+         command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+         options=options
+    )
+
+    browser = Browser(Config(driver))
+
+    # browser.config.base_url = 'https://demoqa.com'
+    # browser.config.hold_browser_open = 'True'
+    # browser.config.window_width = 1600
+    # browser.config.window_height = 1000
+
+    yield browser
+
+    add_html(browser)
+    add_screenshot(browser)
+    add_logs(browser)
+    # add_video(browser)
+    browser.quit()
+    # shutil.rmtree('allure-results')
--- tests/test_demoqa_check_form.py
+++ tests/test_demoqa_check_form.py
@@ -1,3 +1,6 @@
+import allure
+from allure_commons.types import Severity
+
 from demoqa_tests.data import User
 from demoqa_tests.model import app
 
@@ -14,38 +17,70 @@ user = User(
     current_address='World',
     state='NCR',
     city='Delhi'
-    )
+)
 
-
-def test_submit_form():
+@allure.severity(Severity.CRITICAL)
+@allure.label('owner', 'juliamur')
+@allure.feature('Registration form')
+@allure.link('https://demoqa.com', name='Test link')
+@allure.title('Successful fill in the form')
+def test_submit_form(setup_browser):
     # PRECONDITION
-    app.given_opened_practice_form()
+    with allure.step('Open registrations form'):
+        browser = setup_browser
+        app.given_opened_practice_form(browser)
 
     # WHEN
-    app.form.set_first_name(user.first_name) \
-        .set_last_name(user.last_name) \
-        .set_email(user.email) \
-        .set_gender(user.gender) \
-        .set_mobile_number(user.mobile_number) \
-        .set_date_of_birth(user.date_of_birth) \
-        .set_subjects(user.subjects) \
-        .subjects_should_have(user.subjects) \
-        .set_hobbies(user.hobbies) \
-        .set_photo(user.photo) \
-        .set_current_address(user.current_address) \
-        .set_state(user.state) \
-        .set_city(user.city) \
-        .submit()
+    with allure.step(f'Filling user firstname: {user.first_name}'):
+        app.form.set_first_name(user.first_name)
+
+    with allure.step(f'Filling user lastname: {user.last_name}'):
+        app.form.set_last_name(user.last_name)
+
+    with allure.step(f'Filling user email: {user.email}'):
+        app.form.set_email(user.email)
+
+    with allure.step(f'User gender choice: {user.gender}'):
+        app.form.set_gender(user.gender)
+
+    with allure.step(f'Filling user mobile number: {user.mobile_number}'):
+        app.form.set_mobile_number(user.mobile_number)
+
+    with allure.step(f'Set user date of birth: {user.date_of_birth}'):
+        app.form.set_date_of_birth(user.date_of_birth)
+
+    with allure.step(f'Subject choice: {user.subjects}'):
+        app.form.set_subjects(user.subjects)
+        app.form.subjects_should_have(user.subjects)
+
+    with allure.step(f'Set user hobbies: {user.hobbies}'):
+        app.form.set_hobbies(user.hobbies)
+
+    with allure.step(f'Upload user photo: {user.photo}'):
+        app.form.set_photo(user.photo)
+
+    with allure.step(f'Filling user current address: {user.current_address}'):
+        app.form.set_current_address(user.current_address)
+
+    with allure.step(f'Set user state: {user.state}'):
+        app.form.set_state(user.state)
+
+    with allure.step(f'Set user city: {user.city}'):
+        app.form.set_city(user.city)
+
+    with allure.step(f'Click Submit'):
+        app.form.submit()
 
     # THEN
-    app.result.verify_sent_data(user.full_name(user.first_name, user.last_name),
-                                user.email,
-                                user.gender,
-                                user.mobile_number,
-                                user.date_of_birth,
-                                user.subjects,
-                                user.hobbies,
-                                user.photo,
-                                user.current_address,
-                                user.state_and_city(user.state, user.city)
-                                )
+    with allure.step("Check form results"):
+        app.result.verify_sent_data(user.full_name(user.first_name, user.last_name),
+                                    user.email,
+                                    user.gender,
+                                    user.mobile_number,
+                                    user.date_of_birth,
+                                    user.subjects,
+                                    user.hobbies,
+                                    user.photo,
+                                    user.current_address,
+                                    user.state_and_city(user.state, user.city)
+                                    )
